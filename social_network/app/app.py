521d5dc220436caf713c133d3b16ea474b507310
alice <alice@volsh.org>
2025-04-17 16:12:14 +0300

    .

--- social_network/app/app.py
+++ social_network/app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -7,6 +8,12 @@ import g4f
 from enhanced_generation import EnhancedGeneration
 import asyncio
 import re
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Users/alice/PycharmProjects/SS/social_network/app/app.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 #python social_network/app/app.py
 
@@ -23,31 +30,6 @@ image_providers = [
 g4f.debug.logging = True
 g4f.check_version = False
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'StudySphere_2024_SuperSecretKey_789@#$'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, nullable=False)
-    phone = db.Column(db.String(11), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-@login_manager.user_loader
-def load_user(id):
-    return User.query.get(int(id))
-
 # Маршруты
 @app.route('/')
 def index():
@@ -61,9 +43,9 @@ def landing():
 def textbook():
     return render_template('textbook.html')
 
-# @app.route('/tasks')
-# def tasks():
-#     return render_template('tasks.html')
+@app.route('/tasks')
+def tasks():
+    return render_template('tasks.html')
 
 @app.route('/tasks')
 def show_tasks():
@@ -78,44 +60,6 @@ def task_view():
 @app.route('/support')
 def support():
     return render_template('support.html')
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'GET':
-        return render_template('register.html')
-    
-    data = request.get_json()
-    if User.query.filter_by(username=data['username']).first():
-        return jsonify({'success': False, 'message': 'Пользователь уже существует'})
-    
-    user = User(username=data['username'], phone=data['phone'])
-    user.set_password(data['password'])
-    db.session.add(user)
-    db.session.commit()
-    return jsonify({'success': True})
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        return render_template('login.html')
-    
-    data = request.get_json()
-    user = User.query.filter_by(username=data['username']).first()
-    
-    if user and user.check_password(data['password']):
-        login_user(user)
-        return jsonify({'success': True})
-    return jsonify({'success': False, 'message': 'Неверное имя пользователя или пароль'})
-
-@app.route('/profile')
-@login_required
-def profile():
-    return render_template('profile.html', user=current_user)
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
 
 def is_valid_image_url(url: str) -> bool:
     """Проверяет валидность URL изображения"""
