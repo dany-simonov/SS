513c29e3d773ef80ea51a82dfde3657cb70a0227
[Dany] <[dany.ssimon2007@yandex.ru]>
2025-04-18 14:26:57 +0300

    Cross-platform

--- social_network/app/__init__.py
+++ social_network/app/__init__.py
@@ -9,7 +9,8 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('instance.config')  # Загрузка конфигурации
+    # app.config.from_object('instance.config')  # Загрузка конфигурации
+    app.config.from_object('social_network.app.instance.config')
 
     db.init_app(app)
     migrate.init_app(app, db)
--- social_network/app/instance/config.py
+++ social_network/app/instance/config.py
@@ -1,6 +1,7 @@
 import os
 
 SECRET_KEY = os.urandom(24)
-SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/app.db'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/app.db'
+SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join('instance', 'app.db')}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DEBUG = True
