d512aa03c29d28bf147f4d11615f8972bbc4ab11
alice <alice@volsh.org>
2025-04-22 17:38:31 +0300

    add email

--- social_network/app/models.py
+++ social_network/app/models.py
@@ -8,6 +8,7 @@ db = SQLAlchemy()
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     phone = db.Column(db.String(11), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
     messages = db.relationship('Message', backref='author', lazy='dynamic')
